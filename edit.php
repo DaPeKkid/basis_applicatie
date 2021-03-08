394f32d76337c88bdfd1bbcdf227b70c05440b29
Wessel ツ <46527052+DaPeKkid@users.noreply.github.com>
2021-03-08 21:34:06 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<a class="nav-link" href="{{ path('app_register') }}"><span class="fas fa-user"></span>Sign Up</a>
+<input type="button" value="Afdrukken" onclick="print()">
+
+
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">ReMaS</a>
         <ul class="navbar-nav px-3">
